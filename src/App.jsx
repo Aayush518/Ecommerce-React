e1a39c9620239a0c95bb5f8c89617ceb9e239dd9
Aayush Adhikari <adhikariaayush37@gmail.com>
2025-01-10 20:55:13 +0545

    Addn fltration (#15)
    * Solve issues on addnfltration
    
    * Solve issues on addnfltration

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import SupportedBy from './Components/SupportedBy/SupportedBy.jsx';
 import BrandsCards from './Components/BrandsCards/BrandsCards.jsx';
 import Bags from './Components/Bags/Bags.jsx';
 import HoldingBags from './Components/HoldingBags/HoldingBags.jsx';
-import Wishlist from './Components/WishList/Wishlist.jsx'; 
+import Wishlist from './Components/Wishlist/Wishlist.jsx'; 
 import Footer from './Components/Footer/Footer.jsx';
 import { CartProvider } from './context/CartContext';
 
